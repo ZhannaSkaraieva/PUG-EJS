8dee0de2b995d6fb0adaeb789f9a7002c7f282ac
Zhanna Skaraieva <i.m.p.h.e.l.l@gmail.com>
2025-05-11 23:54:54 +0300

    changed file extension

--- app.js
+++ app.js
@@ -7,40 +7,46 @@ const app = express();
 require('dotenv').config(); // загрузка переменных окружения из .env файла
 const router = express.Router();
 const { engine } = require('express-handlebars'); // подключение 
+const path = require('path');
+
+//НАСТРОЙКА Express.js РАБОТАТЬ СО СТАТИЧНЫМИ ФАЙЛАМИ В ПАПКЕ public.
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 // // НАСТРОЙКА МАРШРУТИЗАЦИИ
 // app.use('/user', router);
 
-// настраиваю движок просмотра для handlebars
+// НАВСТРОЙКА движока просмотра для handlebars
 app.engine('handlebars', engine(
   {
-//настрйка путей для главного макета 
+//НАСТРОЙКА ПУТЕЙ ДЛЯ ГЛАВНОГО МАЕТА 
   defaultLayout: 'users',             // главный макет по умолчанию
-  layoutsDir: 'views/layouts',     // папка с макетами
-  partialsDir: 'views/partials'    // папка с частичными шаблонами (header, nav и т.д.)
-}
+  layoutsDir: path.join(__dirname, 'views/layouts'),// главный макет для рендеринга
+  partialsDir: path.join(__dirname, 'views/partials'), // папка с частичными шаблонами (header, nav и т.д.)
+}      
 ));
 app.set('view engine', 'handlebars');
 // в EXPRESS указываем местоположение файлов для компиляции
 app.set('views', './views');
 
+//СОЗДАНИЕ ПОЛЬЗОВАТЕЛЬ. РАБОТА БЕЗ ПОДКЛЮЧЕНИЯ К БАЗЕ
 const users = [
   { id: 1, name: 'Alex', email: 'alex@example.com' },
   { id: 2, name: 'Yevhenii', email: 'eugene@example.com' }
 ];
 
-// Маршрут для головної сторінки
+//МАРШРУТ ДЛЯ ГЛАВОЙ СТРАНИЧКИ
 app.get('/users', (req, res) => {
-  res.render('allUsers.hbs', { title: 'Все пользователи', users }); //функция response.render() создают страницу html и отправляет 
-  // ее пользователю с конткнтом прописаным в файле allUsers.hbs 
+  res.render('allUsers.handlebars', { title: 'Все пользователи', users }); //функция response.render() создают страницу html и отправляет 
+// ее пользователю с конткнтом прописаным в файле allUsers.hbs 
 });
 
-//маршрут для страницы вызовов по id
+//МАРШРУТ ДЛЯ СТРАНИЦЫ ВЫЗОВОВ ПО id
 app.get('/users/:userId', (req, res) => {
   const user = users.find(user => user.id == req.params.userId); 
   if (!user) return res.status(404).send('User not found');
   const title = `Пользователь: ${user.name}`;
-  res.render('user.hbs', { title, user });
+  res.render('user.handlebars', { title, user });
 });
 
 
